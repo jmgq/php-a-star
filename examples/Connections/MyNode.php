ade8bfe058859dcfba9fb5d1cbc3bc2653586d1b
Jose Gonzalez <jmgq@jmgq.es>
2014-06-01 22:26:34 +0100

    Add Connections\MyNode

---
+++ examples/Connections/MyNode.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace JMGQ\AStar\Example\Connections;
+
+use JMGQ\AStar\AbstractNode;
+use JMGQ\AStar\Node;
+
+class MyNode extends AbstractNode
+{
+    private $x;
+    private $y;
+
+    public function __construct($x, $y)
+    {
+        $this->x = $this->filterInteger($x);
+        $this->y = $this->filterInteger($y);
+    }
+
+    /**
+     * @param Node $node
+     * @return MyNode
+     */
+    public static function fromNode(Node $node)
+    {
+        $coordinates = explode('x', $node->getID());
+
+        if (count($coordinates) != 2) {
+            throw new \InvalidArgumentException('Invalid node: ' . print_r($node, true));
+        }
+
+        $x = $coordinates[0];
+        $y = $coordinates[1];
+
+        return new MyNode($x, $y);
+    }
+
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getID()
+    {
+        return $this->x . 'x' . $this->y;
+    }
+
+    private function filterInteger($value)
+    {
+        $integer = filter_var($value, FILTER_VALIDATE_INT);
+
+        if ($integer === false) {
+            throw new \InvalidArgumentException('Invalid integer: ' . print_r($value, true));
+        }
+
+        return $integer;
+    }
+}
---
+++ tests/Example/Connections/MyNodeTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace JMGQ\AStar\Tests\Example\Connections;
+
+use JMGQ\AStar\Example\Connections\MyNode;
+
+class MyNodeTest extends \PHPUnit_Framework_TestCase
+{
+    public function validPointProvider()
+    {
+        $PHP_INT_MIN = ~PHP_INT_MAX;
+
+        return array(
+            array(3, 4),
+            array(0, 3),
+            array('1', '2'),
+            array('2', 2),
+            array($PHP_INT_MIN, PHP_INT_MAX),
+            array(-1, 3),
+            array('-2', -8),
+            array(4, -7)
+        );
+    }
+
+    public function invalidPointProvider()
+    {
+        return array(
+            array(2.3, 2),
+            array(4, null),
+            array(null, 2),
+            array('a', 2),
+            array(array(), false)
+        );
+    }
+
+    public function testShouldImplementTheNodeInterface()
+    {
+        $sut = new MyNode(0, 0);
+
+        $this->assertInstanceOf('JMGQ\AStar\Node', $sut);
+    }
+
+    /**
+     * @dataProvider validPointProvider
+     */
+    public function testShouldSetValidPoint($x, $y)
+    {
+        $expectedX = (int) $x;
+        $expectedY = (int) $y;
+
+        $sut = new MyNode($x, $y);
+
+        $this->assertSame($expectedX, $sut->getX());
+        $this->assertSame($expectedY, $sut->getY());
+    }
+
+    /**
+     * @dataProvider invalidPointProvider
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid integer
+     */
+    public function testShouldNotSetInvalidPoint($x, $y)
+    {
+        new MyNode($x, $y);
+    }
+
+    /**
+     * @dataProvider validPointProvider
+     */
+    public function testShouldGenerateAnID($x, $y)
+    {
+        $expectedID = $x . 'x' . $y;
+
+        $sut = new MyNode($x, $y);
+
+        $this->assertSame($expectedID, $sut->getID());
+    }
+
+    public function testShouldCreateNewInstanceFromNode()
+    {
+        $x = -3;
+        $y = 5;
+        $nodeID = $x . 'x' . $y;
+
+        $node = $this->getMock('JMGQ\AStar\Node');
+        $node->expects($this->once())
+            ->method('getID')
+            ->will($this->returnValue($nodeID));
+
+        $myNode = MyNode::fromNode($node);
+
+        $this->assertSame($x, $myNode->getX());
+        $this->assertSame($y, $myNode->getY());
+        $this->assertSame($nodeID, $myNode->getID());
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Invalid node
+     */
+    public function testShouldNotCreateNewInstanceFromInvalidNode()
+    {
+        $nodeID = 'foo';
+
+        $node = $this->getMock('JMGQ\AStar\Node');
+        $node->expects($this->once())
+            ->method('getID')
+            ->will($this->returnValue($nodeID));
+
+        MyNode::fromNode($node);
+    }
+}
